c8e4fbf04cd5b76a452b819142c23befb014940c
Vincent Rémond <vincent.remond@gmail.com>
2020-12-09 17:09:58 +0100

    test

---
+++ HudMicSwitch/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace HudMicSwitch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MicAccess.MicOn();
+        }
+
+        private void btnBTW_Click(object sender, EventArgs e)
+        {
+            MicAccess.MicOff();
+        }
+    }
+}
---
+++ HudMicSwitch/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HudMicSwitch
+{
+    public static class Helper
+    {
+        public static float Percent(this int value)
+        {
+            if (value < 0 || value > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            return 0.01f * value;
+        }
+    }
+}
