49b96e4c3e8a20eccfe6bfba0df37a5d03361abc
Vincent Rémond <vincent.remond@gmail.com>
2024-12-30 09:10:04 +0100

    quickstart

--- HudMicSwitch.Lib/VoiceMeeterClient.cs
+++ HudMicSwitch.Lib/VoiceMeeterClient.cs
@@ -1,60 +1,59 @@
 ﻿using System;
 using Microsoft.Extensions.Logging;
 
-namespace HudMicSwitch.Lib
+namespace HudMicSwitch.Lib;
+
+public class VoiceMeeterClient : IDisposable
 {
-    public class VoiceMeeterClient : IDisposable
+    private readonly ILogger<VoiceMeeterClient>? _logger;
+
+    public VoiceMeeterClient(ILogger<VoiceMeeterClient>? logger)
     {
-        private readonly ILogger<VoiceMeeterClient>? _logger;
+        _logger = logger;
+        Login();
+    }
 
-        public VoiceMeeterClient(ILogger<VoiceMeeterClient>? logger)
+    private void Login()
+    {
+        Logout();
+        var loginResponse = VoiceMeeterRemote.Login();
+
+        var info = loginResponse switch
         {
-            _logger = logger;
-            Login();
-        }
+            VbLoginResponse.Ok => "Attached.",
+            VbLoginResponse.OkVoiceMeeterNotRunning => "Attached but VoiceMeeter is not running.",
+            VbLoginResponse.AlreadyLoggedIn => "Attached. Was already logged in",
+            VbLoginResponse.NoClient => "No client",
+            _ => throw new InvalidOperationException($"Bad response from VoiceMeeter: {loginResponse}"),
+        };
+        _logger?.LogInformation(info);
+    }
 
-        private void Login()
-        {
-            Logout();
-            var loginResponse = VoiceMeeterRemote.Login();
+    public void SetParam(string n, float v)
+    {
+        VoiceMeeterRemote.SetParameter(n, v);
+    }
 
-            var info = loginResponse switch
-            {
-                VbLoginResponse.Ok => "Attached.",
-                VbLoginResponse.OkVoiceMeeterNotRunning => "Attached but VoiceMeeter is not running.",
-                VbLoginResponse.AlreadyLoggedIn => "Attached. Was already logged in",
-                VbLoginResponse.NoClient => "No client",
-                _ => throw new InvalidOperationException($"Bad response from VoiceMeeter: {loginResponse}"),
-            };
-            _logger?.LogInformation(info);
-        }
+    public void SetParam(string n, string v)
+    {
+        VoiceMeeterRemote.SetParameter(n, v);
+    }
 
-        public void SetParam(string n, float v)
-        {
-            VoiceMeeterRemote.SetParameter(n, v);
-        }
+    public float GetParam(string n)
+    {
+        float output = -1;
+        VoiceMeeterRemote.GetParameter(n, ref output);
+        return output;
+    }
 
-        public void SetParam(string n, string v)
-        {
-            VoiceMeeterRemote.SetParameter(n, v);
-        }
+    public void Dispose()
+    {
+        Logout();
+    }
 
-        public float GetParam(string n)
-        {
-            float output = -1;
-            VoiceMeeterRemote.GetParameter(n, ref output);
-            return output;
-        }
-
-        public void Dispose()
-        {
-            Logout();
-        }
-
-        private void Logout()
-        {
-            var logoutResponse = VoiceMeeterRemote.Logout();
-            _logger?.LogInformation($"Logged out {logoutResponse}");
-        }
+    private void Logout()
+    {
+        var logoutResponse = VoiceMeeterRemote.Logout();
+        _logger?.LogInformation($"Logged out {logoutResponse}");
     }
 }
