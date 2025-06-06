08a3876f4c010cc279a5ae81817b49c5f0a829a0
Vincent Rémond <vincent.remond@gmail.com>
2025-06-06 23:49:27 +0200

    quickstart

--- VoiceMeeter.TestApp/Form1.cs
+++ VoiceMeeter.TestApp/Form1.cs
@@ -3,36 +3,35 @@ using System;
 using System.Windows.Forms;
 using HudMicSwitch.Lib;
 
-namespace VoiceMeeterTest
+namespace VoiceMeeterTest;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private readonly VoiceMeeterClient _client;
+
+    public Form1()
     {
-        private readonly VoiceMeeterClient _client;
+        InitializeComponent();
 
-        public Form1()
+        _client = new VoiceMeeterClient(null);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        var key = textBoxKey.Text;
+        var value = textBoxValue.Text;
+        if (float.TryParse(value, out var valueFloat))
         {
-            InitializeComponent();
-
-            _client = new VoiceMeeterClient(null);
+            _client.SetParam(key, valueFloat);
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        else
         {
-            var key = textBoxKey.Text;
-            var value = textBoxValue.Text;
-            if (float.TryParse(value, out var valueFloat))
-            {
-                _client.SetParam(key, valueFloat);
-            }
-            else
-            {
-                _client.SetParam(key, value ?? string.Empty);
-            }
+            _client.SetParam(key, value ?? string.Empty);
         }
+    }
 
-        private void Form1_Closed(object? sender, EventArgs e)
-        {
-            _client.Dispose();
-        }
+    private void Form1_Closed(object? sender, EventArgs e)
+    {
+        _client.Dispose();
     }
 }
